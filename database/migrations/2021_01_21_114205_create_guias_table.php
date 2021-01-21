d2020b8d8ec34eed3c231abf12e652495c9435e8
wverau <wverau@gmail.com>
2021-01-21 12:06:13 -0500

    Migraciones
    Agrego las misgraciones

---
+++ database/migrations/2021_01_21_114205_create_guias_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGuiasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('guias', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('producto_id')->unsigned();
+            // $table->foreign('producto_id')
+            //       ->references('id')->on('productos');
+            //       ->onDelete('cascade');
+            $table->unsignedInteger('unidades_a_producir')->unsigned();
+            $table->string('area_responsable1')->nullable();
+            $table->string('area_responsable2')->nullable();
+            $table->date('fecha')->nullable();
+            $table->string('observacion')->nullable();
+            $table->BigInteger('usuario_id');
+            $table->enum('estado',['ACTIVA','ANULADA'])->default('ACTIVA');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('guias');
+    }
+}
