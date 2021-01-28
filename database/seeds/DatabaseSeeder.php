10697b0d28ddd8f4fd6a128fc92085faed010a49
Axel <axelgutierrezlopez26@gmail.com>
2021-01-28 16:09:49 -0500

    seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -7,7 +7,6 @@ class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        $this->call(EmpresaSeeder::class);
         $this->call(DepartamentoSeeder::class);
         $this->call(ProvinciaSeeder::class);
         $this->call(DistritoSeeder::class);
@@ -15,7 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call(TablaSeeder::class);
         $this->call(TablaDetalleSeeder::class);
         $this->call(ParametroSeeder::class);
-        
+        $this->call(EmpresaSeeder::class);
         // $this->call(DatosPruebaSeeder::class);
 
 
--- database/seeds/EmpresaSeeder.php
+++ database/seeds/EmpresaSeeder.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Database\Seeder;
 use App\Mantenimiento\Empresa\Empresa;
+use App\Compras\Proveedor;
+
+use App\Ventas\Cliente;
+use App\Mantenimiento\Persona\Persona;
+use App\Mantenimiento\Empleado\Empleado;
 
 class EmpresaSeeder extends Seeder
 {
@@ -25,5 +30,63 @@ class EmpresaSeeder extends Seeder
         $empresa->num_partida = '11036086';
         $empresa->activo = '1';
         $empresa->save();
+
+
+        $proveedor = new Proveedor();
+        $proveedor->ruc = '20604793310';
+        $proveedor->descripcion = 'LIMPIATODO S.A.C';
+        $proveedor->tipo_documento = 'RUC';
+        $proveedor->tipo_persona = 'PERSONA JURIDICA';
+        $proveedor->direccion = 'Jr. Puerto Inca Nro. 250 Dpto. 402';
+        $proveedor->correo = 'AXELGUTIERREZLOPEZ26@GMAIL.COM';
+        $proveedor->telefono = '043313520';
+        $proveedor->zona = 'NOROESTE';
+        $proveedor->contacto = 'LIZ FLORES';
+        $proveedor->telefono_contacto = '957819664';
+        $proveedor->correo_contacto = 'AXELGUTIERREZLOPEZ26@GMAIL.COM';
+        $proveedor->transporte = 'SEDACHIMBOTE S.A.';
+        $proveedor->ruc_transporte = '20136341066';
+        $proveedor->direccion_transporte = 'JR. LA CALETA NRO. 176 A.H.  MANUEL SEOANE CORRALES - ANCASH - SANTA - CHIMBOTE';
+
+        $proveedor->activo_transporte = '1';
+        $proveedor->activo = '1';
+        $proveedor->save();
+
+        // $persona = new Persona();
+        // $persona->tipo_documento = 'DNI';
+        // $persona->documento = '10000000';
+        // $persona->nombres = 'AXEL GASTON KEVIN';
+        // $persona->apellido_paterno = 'GUTIERREZ';
+        // $persona->apellido_materno = 'LOPEZ';
+        // $persona->fecha_nacimiento = '2021-01-06';
+        // $persona->sexo = 'M';
+        // $persona->estado = 'ACTIVO';
+        // $persona->save();
+
+
+        // $empleado = new Empleado();
+        // $empleado->persona_id = 1;
+        // $empleado->area = '1';
+        // $empleado->profesion = '1';
+        // $empleado->cargo = '1';
+        // $empleado->sueldo = '1000';
+        // $empleado->sueldo_bruto = '1000';
+        // $empleado->sueldo_neto = '10000';
+        // $empleado->moneda_sueldo = 'SOLES';
+        // $empleado->fecha_inicio_actividad = '2021-01-06';
+        // $empleado->estado = 'ACTIVO';
+        // $empleado->save();
+
+        // $cliente = new Cliente();
+        // $cliente->tipo_documento = '70004110';
+        // $cliente->documento = 'DNI';
+        // $cliente->direccion_negocio = 'JR. LA CALETA NRO. 176 A.H.  MANUEL SEOANE CORRALES - ANCASH - SANTA - CHIMBOTE';
+        // $cliente->direccion = 'JR. LA CALETA NRO. 176 A.H.  MANUEL SEOANE CORRALES - ANCASH - SANTA - CHIMBOTE';
+        // $cliente->correo_electronico = 'AXELGUTIERREZLOPEZ26@GMAIL.COM';
+        // $cliente->telefono_movil = '1345678';
+        // $cliente->activo = '1';
+        // $cliente->save();
+
+
     }
 }
